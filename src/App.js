028a80e147de83c9b24a53d0d59a427a4be80f23
jks <jksjhakash@gmail.com>
2023-09-29 16:29:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  const[name,setName]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPswd]=useState("");
 
-export default App;
+  const handal=()=>{
+    localStorage.setItem("name", name)
+    localStorage.setItem("email", email)
+    localStorage.setItem("password", password)
+    setName('');
+    setEmail('');
+    setPswd('');
+  }
+     return(
+        <div className="box">
+          <label className="text">NAME :</label>
+          <input type="text" className="textarea" placeholder="ENTER YOUR NAME" onChange={(e)=>setName(e.target.value)} value={name}/>
+          <label className="text">EMAIL :</label>
+          <input type="text" className="textarea" placeholder="ENTER YOUR E-MAIL" onChange={(e)=>setEmail(e.target.value)} value={email}/>
+          <label className="text">PASSWORD :</label>
+          <input type="password" className="textarea" placeholder="ENTER YOUR PASSWORD" onChange={(e)=>setPswd(e.target.value)} value={password}/><p></p>
+    
+          <button className="btn" onClick={handal}>Hit Me</button>
+          <div className="output">
+            <table>
+              <tr>
+                <th> NAME &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th>E-MAIL &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th>PASSWORD</th>
+              </tr>
+              <tr>
+                <td><div>{localStorage.getItem("name")}</div></td>
+                <td><div>{localStorage.getItem("email")}</div></td>
+                <td><div>{localStorage.getItem("password")}</div></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      )
+}
+export default App
